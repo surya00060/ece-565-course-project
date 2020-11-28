ba3afd72dac2ec59f96266d9367c17e26f8ba958
surya00060 <surya00060@gmail.com>
2020-11-28 18:50:07 +0530

    c++: added a first version of VP

---
+++ src/cpu/vpred/lvp.cc
@@ -0,0 +1,86 @@
+#include "cpu/vpred/lvp.hh"
+
+#include "base/intmath.hh"
+// #include "base/logging.hh"
+// #include "base/trace.hh"
+// #include "debug/Fetch.hh"
+
+LVP::LVP(const LVPParams *params)
+    : SimObject(params),
+      lastPredictorSize(params->lastPredictorSize),
+      lastCtrBits(params->lastCtrBits),
+      classificationTable(lastPredictorSize, SatCounter(lastCtrBits)),
+      valuePredictionTable(lastPredictorSize)
+{
+    // valuePredictionTable.resize(lastPredictorSize);
+}
+
+void 
+LVP::regStats()
+{
+    SimObject::regStats();
+    lookups
+        .name(name() + ".lookups")
+        .desc("Number of VP lookups")
+        ;
+
+    numPredicted
+        .name(name() + ".numPredicted")
+        .desc("Number of value predictions")
+        ;
+
+    numIncorrectPredicted
+        .name(name() + ".numIncorrectPredicted")
+        .desc("Number of incorrect value predictions")
+        ;
+}
+bool
+LVP::lookup(ThreadID tid, Addr inst_addr, RegVal &value)
+{
+    ++lookups;
+
+    unsigned index = inst_addr%lastPredictorSize;
+
+    uint8_t counter_val = classificationTable[index];
+
+    /*Gets the MSB of the count.*/
+    bool prediction = counter_val >> (lastCtrBits-1);
+
+    if (prediction)
+    {
+        ++numPredicted;
+        value = valuePredictionTable[index];
+    }
+
+    return prediction;
+}
+
+void
+LVP::update(ThreadID tid, Addr inst_addr, bool taken, bool squashed, RegVal &value)
+{
+    if (squashed)
+    {
+        return;
+    }
+    unsigned index = inst_addr%lastPredictorSize;
+
+    if (taken)
+    {
+        // The Value predicted and True values are the same.
+        classificationTable[index]++; 
+    }
+    else
+    {
+        // Decrease the counter and update the value to prediction table.
+        ++numIncorrectPredicted;
+        classificationTable[index]++;
+        valuePredictionTable[index] = value;
+    }
+}
+
+// LVP*
+// LVPParams::create()
+// {
+//     return new LVP(this);
+// }
+
---
+++ src/cpu/vpred/lvp.hh
@@ -0,0 +1,44 @@
+#ifndef __CPU_VPRED_LVP_PRED_HH__
+#define __CPU_VPRED_LVP_PRED_HH__
+
+#include <vector>
+
+#include "base/statistics.hh"
+#include "base/sat_counter.hh"
+#include "base/types.hh"
+#include "cpu/inst_seq.hh"
+#include "cpu/static_inst.hh"
+#include "params/LVP.hh"
+#include "sim/sim_object.hh"
+
+class LVP : public SimObject
+{
+    public:
+        
+        LVP(const LVPParams *params);
+
+        void regStats() override;
+
+        bool lookup(ThreadID tid, Addr inst_addr, RegVal &value);
+
+        void update(ThreadID tid, Addr inst_addr, bool taken, bool squashed, RegVal &value);
+    
+    private:
+        /** Number of Table Entries*/
+        const unsigned lastPredictorSize;
+
+        /** Number of bits to control*/
+        const unsigned lastCtrBits;
+
+        /*Array of counters*/
+        std::vector<SatCounter> classificationTable;
+
+        /*Array of value predictions*/
+        std::vector<RegVal> valuePredictionTable;
+
+        Stats::Scalar lookups;
+        Stats::Scalar numPredicted;
+        Stats::Scalar numIncorrectPredicted;
+
+};
+#endif // __CPU_VPRED_LVP_PRED_HH__
