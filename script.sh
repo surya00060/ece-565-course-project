164a876b76688b08b7b207697d0787ac2469c06f
surya <surya00060@gmail.com>
2020-12-10 20:34:12 +0530

    ADded Script. Was not able to figure out how to parameterize dfrom Script

--- script.sh
+++ script.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-# Script to run SPEC benchmarks: Baseline
-# ISA: ARM; CPU: DerivO3; System Parameters: Default; No.of Ins: 1 Billion; Warmup Ins: 100 Million; AtomicCPU Ins: 1 Billion
-# Paper 1, Best performing Benchmarks [Few of them from SPEC92]: mpeg, gperf, eqntott[S], sc, gawk, cc1[S], compress[S], xlisp[S], grep 
+# Paper 1, Best performing Benchmarks [Few of them from SPEC92]: mpeg, gperf, eqntott[S], sc, gawk, cc1[S], compress[S], xlisp[S], grep
 # Paper 2, SPEC95 = compress, gcc, go, ijpeg, m88k, perl, lisp
 # Paper 3, Best performing benchmarks SPEC2K6: bzip2, gamess, h264, gcc, mcf, hmmer OtherBench: gobmk, milc, sjeng, lbm
-benchmarks=(bzip2 gamess h264ref gcc mcf hmmer gobmk milc sjeng lbm)
-
+#benchmarks=(bzip2 gamess h264ref gcc mcf hmmer gobmk milc sjeng lbm)
+benchmarks=(lbm milc sjeng)
 for b in "${benchmarks[@]}"; do
-    folder="baseline_"$b
-    echo $b, $folder
-    ./build/ARM/gem5.opt -d results/$folder configs/spec2k6/run.py -b $b --maxinsts=1000000000 --standard-switch=1000000000 --warmup-insts=50000000 --cpu-type=DerivO3CPU --caches --l2cache &
-done 
+    folder="VPTest_"$b
+    ./build/ARM/gem5.opt -d $folder configs/spec2k6/run.py -b $b --maxinsts=1000000000 --warmup-insts=50000000 --cpu-type=TimingSimpleCPU
+done
